5a3736f954856193b81e0a136746a06badbc6f0f
Rafael Silva <rafael.silva@commsl.com.br>
2021-11-19 22:14:53 -0300

    criação projeto teste de unidade

--- ChallengeFiap.Data/Migrations/20211120003414_EventoMigration.cs
+++ ChallengeFiap.Data/Migrations/20211120003414_EventoMigration.cs
@@ -6,7 +6,7 @@ namespace ChallengeFiap.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-          migrationBuilder.cre
+          
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
---
+++ ChallengerFiap.Teste/EventoTeste.cs
@@ -0,0 +1,38 @@
+﻿using ChallengeFiap.Model;
+using ChallengeFiap.Services.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ChallengerFiap.Teste
+{
+  public class EventoTeste
+  {
+    IEventoService _eventoService;
+    public EventoTeste(IEventoService eventoService)
+    {
+      _eventoService = eventoService;
+    }
+
+    [Test]
+    public void DeveriaCriarEvento()
+    {
+      var evento = new Evento();
+      evento.Id = 1;
+      evento.Gratuito = false;
+      evento.Local = "Av. Paulista 910";
+      evento.Data = DateTime.Now;
+      evento.Descricao = "Evento de teste da fiap";
+      evento.Nome = "Challanger fiap";
+      evento.QuantidadeMaximaPessoas = 100;
+      evento.QuantidadePessoasConfirmada = 0;
+      evento.QuantidadePessoasTalvez = 0;
+
+      var resultado = _eventoService.CriarEvento(evento);
+
+      Assert.IsTrue(resultado);
+    }
+  }
+}
