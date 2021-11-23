ed10230fd6aa9d3070ca4dcdcd2715f132f83b6e
Lucas Coutinho <lucascgs1@hotmail.com>
2021-11-22 21:36:03 -0300

    ajusta ta home de eventos

--- ChallengeFiap.Api/Controllers/ContaController.cs
+++ ChallengeFiap.Api/Controllers/ContaController.cs
@@ -15,110 +15,139 @@ using ChallengeFiap.Model;
 
 namespace ChallengeFiap.Api.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class ContaController : ControllerBase
-  {
-    private readonly IOptions<AppSettings> _appSettings;
-
-
-    public ContaController(
-      IOptions<AppSettings> appSettings
-    )
+    /// <summary>
+    /// controller de configuracao de contas
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContaController : ControllerBase
     {
-      _appSettings = appSettings;
+        private readonly IOptions<AppSettings> _appSettings;
+
+        /// <summary>
+        /// construtor
+        /// </summary>
+        /// <param name="appSettings"></param>
+        public ContaController(
+          IOptions<AppSettings> appSettings
+        )
+        {
+            _appSettings = appSettings;
+        }
+
+        /// <summary>
+        /// Login na aplicacao
+        /// </summary>
+        /// <param name="usuarioServices">servico de usuario</param>
+        /// <param name="login">dados de login</param>
+        /// <returns>usuario e token de autenticacao</returns>
+        [HttpPost("autenticar")]
+        [AllowAnonymous]
+        public async Task<ActionResult> Autenticar([FromServices] IUsuarioServices usuarioServices, [FromBody] LoginViewModel login)
+        {
+            try
+            {
+                var usuario = await usuarioServices.AuthenticateAsync(login.Email, login.Senha);
+
+                if (usuario == null)
+                    throw new ValidationException("Usuário ou senha inválidos!");
+
+                if (usuario.Senha != login.Senha.Md5Hash())
+                    throw new ValidationException("Senha invalida!");
+
+                var token = TokenHelper.GenerateToken(usuario, _appSettings.Value.Secret);
+
+                return Ok(new { usuario, token });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Obtem os dados do usuario
+        /// </summary>
+        /// <param name="usuarioServices">servico de usuario</param>
+        /// <returns>dados do usuario</returns>
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult> Get([FromServices] IUsuarioServices usuarioServices)
+        {
+            try
+            {
+                var userId = this.User.Claims.Where(x => x.Type.Contains("authentication")).FirstOrDefault().Value;
+                var usuario = await usuarioServices.GetByIdAsync(Convert.ToInt32(userId));
+
+                return Ok(new { usuario });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// edita os dados do usuario
+        /// </summary>
+        /// <param name="usuarioServices">servico de usuario</param>
+        /// <param name="usuarioViewModel">modelo de dados do usuario</param>
+        /// <returns></returns>
+        [HttpPost("editar")]
+        [Authorize]
+        public async Task<ActionResult> Editar([FromServices] IUsuarioServices usuarioServices, [FromBody] UsuarioViewModel usuarioViewModel)
+        {
+            try
+            {
+                var userId = this.User.Claims.Where(x => x.Type.Contains("authentication")).FirstOrDefault().Value;
+                var usuario = await usuarioServices.GetByIdAsync(Convert.ToInt32(userId));
+
+                usuario.Nome = usuarioViewModel.Nome;
+                usuario.Sobrenome = usuarioViewModel.Sobrenome;
+                usuario.Apelido = usuarioViewModel.Apelido;
+                usuario.Email = usuarioViewModel.Email;
+                usuario.Senha = usuarioViewModel.Senha;
+                usuario.Telefone = usuarioViewModel.Telefone;
+
+                usuarioServices.Save(usuario, usuario.Id);
+
+                return Ok(new { usuario });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// efetua o cadastro de um novo usuario
+        /// </summary>
+        /// <param name="usuarioServices">servico de usuarios</param>
+        /// <param name="usuarioViewModel">dados do usuario</param>
+        /// <returns></returns>
+        [HttpPost("cadastrar")]
+        public ActionResult Cadastrar([FromServices] IUsuarioServices usuarioServices, [FromBody] UsuarioViewModel usuarioViewModel)
+        {
+            try
+            {
+                var usuario = new Usuario();
+
+                usuario.Nome = usuarioViewModel.Nome;
+                usuario.Sobrenome = usuarioViewModel.Sobrenome;
+                usuario.Apelido = usuarioViewModel.Apelido;
+                usuario.Email = usuarioViewModel.Email;
+                usuario.Senha = usuarioViewModel.Senha;
+                usuario.Telefone = usuarioViewModel.Telefone;
+
+                usuarioServices.Save(usuario, usuario.Id);
+
+                return Ok(new { usuario });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
-
-    [HttpPost("autenticar")]
-    [AllowAnonymous]
-    public async Task<ActionResult> Autenticar([FromServices] IUsuarioServices usuarioServices, [FromBody] LoginViewModel login)
-    {
-      try
-      {
-        var usuario = await usuarioServices.AuthenticateAsync(login.Email, login.Senha);
-
-        if (usuario == null)
-          throw new ValidationException("Usuário ou senha inválidos!");
-
-        if (usuario.Senha != login.Senha.Md5Hash())
-          throw new ValidationException("Senha invalida!");
-
-        var token = TokenHelper.GenerateToken(usuario, _appSettings.Value.Secret);
-
-        return Ok(new { usuario, token });
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(ex.Message);
-      }
-    }
-
-
-    [HttpGet]
-    [Authorize]
-    public async Task<ActionResult> Get([FromServices] IUsuarioServices usuarioServices)
-    {
-      try
-      {
-        var userId = this.User.Claims.Where(x => x.Type.Contains("authentication")).FirstOrDefault().Value;
-        var usuario = await usuarioServices.GetByIdAsync(Convert.ToInt32(userId));
-
-        return Ok(new { usuario });
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(ex.Message);
-      }
-    }
-
-    [HttpPost("editar")]
-    [Authorize]
-    public async Task<ActionResult> Editar([FromServices] IUsuarioServices usuarioServices, [FromBody] UsuarioViewModel usuarioViewModel)
-    {
-      try
-      {
-        var userId = this.User.Claims.Where(x => x.Type.Contains("authentication")).FirstOrDefault().Value;
-        var usuario = await usuarioServices.GetByIdAsync(Convert.ToInt32(userId));
-
-        usuario.Nome = usuarioViewModel.Nome;
-        usuario.Sobrenome = usuarioViewModel.Sobrenome;
-        usuario.Apelido = usuarioViewModel.Apelido;
-        usuario.Email = usuarioViewModel.Email;
-        usuario.Senha = usuarioViewModel.Senha;
-        usuario.Telefone = usuarioViewModel.Telefone;
-
-        usuarioServices.Save(usuario, usuario.Id);
-
-        return Ok(new { usuario });
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(ex.Message);
-      }
-    }
-
-    [HttpPost("cadastrar")]
-    public ActionResult Cadastrar([FromServices] IUsuarioServices usuarioServices, [FromBody] UsuarioViewModel usuarioViewModel)
-    {
-      try
-      {
-        var usuario = new Usuario();
-
-        usuario.Nome = usuarioViewModel.Nome;
-        usuario.Sobrenome = usuarioViewModel.Sobrenome;
-        usuario.Apelido = usuarioViewModel.Apelido;
-        usuario.Email = usuarioViewModel.Email;
-        usuario.Senha = usuarioViewModel.Senha;
-        usuario.Telefone = usuarioViewModel.Telefone;
-
-        usuarioServices.Save(usuario, usuario.Id);
-
-        return Ok(new { usuario });
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(ex.Message);
-      }
-    }
-  }
 }
--- ChallengeFiap.Api/Controllers/EventController.cs
+++ ChallengeFiap.Api/Controllers/EventController.cs
@@ -12,110 +12,137 @@ using System.Threading.Tasks;
 
 namespace ChallengeFiap.Api.Controllers
 {
-  [Route("api/")]
-  [ApiController]
-  public class EventController : ControllerBase
-  {
-
-    [HttpGet]
-    [Route("eventos")]
-    public ActionResult Get([FromServices] IEventoService eventoService)
+    /// <summary>
+    /// controller de eventos
+    /// </summary>
+    [Route("api/")]
+    [ApiController]
+    public class EventController : ControllerBase
     {
-      try
-      {
-        return Ok(eventoService.ListarEventos());
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(ex.Message);
-      }
+        /// <summary>
+        /// obtem os dados de evento
+        /// </summary>
+        /// <param name="eventoService">servico de eventos</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("eventos")]
+        public ActionResult GetAll([FromServices] IEventoService eventoService)
+        {
+            try
+            {
+                return Ok(eventoService.ListarEventos());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// obtem os dados de um evento
+        /// </summary>
+        /// <param name="eventoService">servico de eventos</param>
+        /// <param name="id">codigo do evento</param>
+        /// <returns>dados do evento</returns>
+        [HttpGet]
+        [Route("evento/{id}")]
+        [Authorize]
+        public ActionResult Get([FromServices] IEventoService eventoService, long id)
+        {
+            try
+            {
+                return Ok(eventoService.ListarEventos(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// salva um novo evento
+        /// </summary>
+        /// <param name="eventoService">servico de evento</param>
+        /// <param name="request">dados do evento novo</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("evento/criar")]
+        public ActionResult Post([FromServices] IEventoService eventoService, [FromBody] EventoViewModel request)
+        {
+            try
+            {
+                Evento ev = new Evento();
+
+
+                ev.Categoria = request.Categoria;
+                ev.Data = request.Data;
+                ev.Descricao = request.Descricao;
+                ev.Gratuito = request.Gratuito;
+                ev.Local = request.Local;
+                ev.Nome = request.Nome;
+                ev.Organizador = request.Organizador;
+                ev.Participantes = request.Participantes != null ? request.Participantes : new List<Participante>();
+                ev.QuantidadeMaximaPessoas = request.QuantidadeMaximaPessoas;
+
+                return Ok(eventoService.CriarEvento(ev));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// atualiza os dados de um evento
+        /// </summary>
+        /// <param name="eventoService">servico de evento</param>
+        /// <param name="request">dados do evento</param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("evento/editar")]
+        public ActionResult Put([FromServices] IEventoService eventoService, [FromBody] EventoViewModel request)
+        {
+            try
+            {
+                Evento ev = new Evento();
+                ev.Categoria = request.Categoria;
+                ev.Data = request.Data;
+                ev.Descricao = request.Descricao;
+                ev.Gratuito = request.Gratuito;
+                ev.Local = request.Local;
+                ev.Nome = request.Nome;
+                ev.Organizador = request.Organizador;
+                ev.Participantes = request.Participantes;
+                ev.QuantidadeMaximaPessoas = request.QuantidadeMaximaPessoas;
+                ev.Id = request.Id;
+
+                return Ok(eventoService.EditarEvento(ev));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// deleta um evento
+        /// </summary>
+        /// <param name="eventoService">servico de vento</param>
+        /// <param name="id">codigo de evento</param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("evento/remover/{id}")]
+        public ActionResult Delete([FromServices] IEventoService eventoService, long id)
+        {
+            try
+            {
+                return Ok(eventoService.RemoverEventos(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
-
-
-    [HttpGet]
-    [Route("evento/{id}")]
-    [Authorize]
-    public ActionResult Get([FromServices] IEventoService eventoService, long id)
-    {
-      try
-      {
-        return Ok(eventoService.ListarEventos(id));
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(ex.Message);
-      }
-
-    }
-
-
-    [HttpPost]
-    [Route("evento/criar")]
-    public ActionResult Post([FromServices] IEventoService eventoService, [FromBody] EventoViewModel request)
-    {
-      try
-      {
-        Evento ev = new Evento();
-
-
-        ev.Categoria = request.Categoria;
-        ev.Data = request.Data;
-        ev.Descricao = request.Descricao;
-        ev.Gratuito = request.Gratuito;
-        ev.Local = request.Local;
-        ev.Nome = request.Nome;
-        ev.Organizador = request.Organizador;
-        ev.Participantes = request.Participantes != null ? request.Participantes : new List<Participante>();
-        ev.QuantidadeMaximaPessoas = request.QuantidadeMaximaPessoas;
-
-        return Ok(eventoService.CriarEvento(ev));
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(ex.Message);
-      }
-    }
-
-
-    [HttpPut]
-    [Route("evento/editar")]
-    public ActionResult Put([FromServices] IEventoService eventoService, [FromBody] EventoViewModel request)
-    {
-      try
-      {
-        Evento ev = new Evento();
-        ev.Categoria = request.Categoria;
-        ev.Data = request.Data;
-        ev.Descricao = request.Descricao;
-        ev.Gratuito = request.Gratuito;
-        ev.Local = request.Local;
-        ev.Nome = request.Nome;
-        ev.Organizador = request.Organizador;
-        ev.Participantes = request.Participantes;
-        ev.QuantidadeMaximaPessoas = request.QuantidadeMaximaPessoas;
-        ev.Id = request.Id;
-
-        return Ok(eventoService.EditarEvento(ev));
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(ex.Message);
-      }
-    }
-
-
-    [HttpDelete]
-    [Route("evento/remover/{id}")]
-    public ActionResult Delete([FromServices] IEventoService eventoService, long id)
-    {
-      try
-      {
-        return Ok(eventoService.RemoverEventos(id));
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(ex.Message);
-      }
-    }
-  }
 }
